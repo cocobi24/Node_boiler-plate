5143f1bc0920c4fa04cd645683ba3dbdd7996909
hojin <cacabi24@hanmail.net>
2021-04-20 23:15:49 +0900

    Boiler-plate // Node.js && React _ END

--- server/index.js
+++ server/index.js
@@ -19,8 +19,9 @@ mongoose.connect(config.mongoURI, {
 }).then( () => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요ㅎ'))
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
 
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
 app.post('/api/users/register', (req, res) => {
 
